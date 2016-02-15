249a3da9d9a902883972d4090f7d1b780014df34
Johan <johand@openmailbox.org>
2016-02-14 20:44:28 -0430

    chapter 3

---
+++ chapter 3. types/type_conversion.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+var i int = 42
+var f float64 = float64(i)
+var u uint = uint(f)
+
+func main()  {
+        fmt.Println(i)
+        fmt.Println(f)
+        fmt.Println(u)
+}
